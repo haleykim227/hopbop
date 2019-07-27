a9a58c2a0f3b8cf56f06fba614e9034377c48047
amysywe <source016@gmail.com>
2019-07-27 12:25:09 -0700

    `Got rid of unnecessary comments.

--- hopbop/LoginViewController.swift
+++ hopbop/LoginViewController.swift
@@ -11,9 +11,6 @@ import FBSDKLoginKit
 
 class LoginViewController: UIViewController, LoginButtonDelegate{
     
-    //@IBOutlet var fbLoginButton: FBButton!
-    //@IBOutlet weak var fbLoginButton: FBButton!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -30,71 +27,10 @@ class LoginViewController: UIViewController, LoginButtonDelegate{
         
         let fbLoginButton = FBLoginButton(type: .roundedRect)
         fbLoginButton.center = view.center
-        fbLoginButton.delegate = self
-        //fbLoginButton = fbLoginB
-        //fbLoginB.addTarget(self, action: #selector(self.loginButtonClicked), for: .touchUpInside)
-        //fbLoginButton = fbLoginB
-        //fbLoginButton.center = view.center
-        
-        // Handle clicks on the button
-        //fbLoginButton.addTarget(self, action: #selector(self.loginButtonClicked), for: .touchUpInside)
-        
         view.addSubview(fbLoginButton)
-        //fbLoginButton.center = view.center
-        
-        
     }
-    /*
     
-    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
-        //print("check")
-        if identifier == "loginToMain" {
-            if let accessToken = AccessToken.current {
-                // A user is already logged in
-                print("User \(accessToken.userID) is logged in!")
-                print("segue")
-                return true
-            }
-            print("no segue")
-            return false
-        }
-        return true
-    }
-    */
-    
-    // Once the button is clicked, show the login dialog
-    @objc func loginButtonClicked() {
-        
-        let loginManager = LoginManager()
-        //loginManager.logOut()
-        loginManager.logIn(permissions: ["public_profile"], from: self) { (result, error) in
-            print("login attempt")
-            if let err = error {
-                print(err.localizedDescription)
-            }
-            else {
-                if result!.isCancelled {
-                    print("Login cancelled.")
-                }
-                else {
-                    print("Logged in.")
-                    print("User \(AccessToken.current!.userID) is logged in!")
-                    DispatchQueue.main.async(){
-                        self.performSegue(withIdentifier: "loginToMain", sender: nil)
-                    }
-                    
-                }
-            }
-        }
-        /*
-        if let accessToken = AccessToken.current {
-            print("User \(accessToken.userID) is logged in!")
-            DispatchQueue.main.async(){
-                self.performSegue(withIdentifier: "loginToMain", sender: nil)
-            }
-        }
- */
-    }
+    // FB LoginButtonDelegate Methods
     
     func loginButton(_ loginButton: FBLoginButton, didCompleteWith result: LoginManagerLoginResult?, error: Error?) {
         if let err = error {
@@ -118,17 +54,6 @@ class LoginViewController: UIViewController, LoginButtonDelegate{
     func loginButtonDidLogOut(_ loginButton: FBLoginButton) {
         
     }
-    
-    
-    /*
-    override func viewWillAppear(_ animated: Bool) {
-        let fbLoginB = FBLoginButton(type: .roundedRect)
-        fbLoginB.center = view.center
-        //fbLoginB.addTarget(self, action: #selector(self.loginButtonClicked), for: .touchUpInside)
-        fbLoginButton = fbLoginB
-        //view.addSubview(fbLoginB)
-    }*/
-    
 
     /*
     // MARK: - Navigation
