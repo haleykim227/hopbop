46043c29adeecf499294869452726e7c443f01c8
amysywe <source016@gmail.com>
2019-07-30 18:44:47 -0700

    Made Database Handler functions static.

--- hopbop/DatabaseHandler.swift
+++ hopbop/DatabaseHandler.swift
@@ -13,76 +13,76 @@ class DatabaseHandler{
     // USER
     
     // Adds new user to the user table.
-    func addNewUser(userID: String, platform: String, name: String, profilePic: String) {
+    static func addNewUser(userID: String, platform: String, name: String, profilePic: String) {
         
     }
     
     // Returns current user's info.
-    func getUserInfo(userID: String, platform: String) {
+    static func getUserInfo(userID: String, platform: String) {
         
     }
     
     // Updates user info in user table.
-    func updateUserInfo(userID: String, platform: String, name: String, profilePic: String) {
+    static func updateUserInfo(userID: String, platform: String, name: String, profilePic: String) {
         
     }
     
     // Update user's last rating.
-    func updateUserLastRating(userID: String, platform: String) {
+    static func updateUserLastRating(userID: String, platform: String) {
         
     }
     
     // Returns if user's last rating was within 24 hours.
-    func isLastRatingWithin24Hours(userID: String, platform: String) -> Bool {
+    static func isLastRatingWithin24Hours(userID: String, platform: String) -> Bool {
         return true
     }
     
     // EVENT
     
     // Returns event info by event ID.
-    func getEventByID(eventID: String) {
+    static func getEventByID(eventID: String) {
         
     }
     
     // Add new event.
-    func addNewEvent(eventName: String, locationName: String, address: String, startTime: Date, endTime: Date) {
+    static func addNewEvent(eventName: String, locationName: String, address: String, startTime: Date, endTime: Date) {
         
     }
     
     // Updates event info by event ID.
-    func updateEventByID(eventID: String, eventName: String, locationName: String, address: String, startTime: Date, endTime: Date) {
+    static func updateEventByID(eventID: String, eventName: String, locationName: String, address: String, startTime: Date, endTime: Date) {
         
     }
     
     // Returns array of event ID's in order of their ranking.
-    func getEventIDsByRanking() -> [String]{
+    static func getEventIDsByRanking() -> [String]{
         return []
     }
     
     // Searches for an event by term, and returns the event.
-    func searchForEventByTerm(term: String) {
+    static func searchForEventByTerm(term: String) {
         
     }
     
     // Returns ratings for event with the eventID.
-    func getRatingsForEvent(eventID: String) {
+    static func getRatingsForEvent(eventID: String) {
         
     }
     
     // Returns array of markers for all events tonight.
-    func getMarkersForTonight() -> [EventMarker] {
+    static func getMarkersForTonight() -> [EventMarker] {
         return [EventMarker(id: "12345")]
     }
     
     // RATING
     
     // Returns rating with associated rating ID.
-    func getRatingByID(ratingID: String) {
+    static func getRatingByID(ratingID: String) {
         
     }
     
     // Returns rating for the current user.
-    func getRatingForUser(userID: String, platform: String) {
+    static func getRatingForUser(userID: String, platform: String) {
         
     }
     
