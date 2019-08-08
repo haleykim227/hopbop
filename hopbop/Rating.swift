bfdb2263ec200b8ff655cd57f1cea9fb87496910
amysywe <source016@gmail.com>
2019-08-07 21:33:03 -0700

    Added Rating and User class.

---
+++ hopbop/Rating.swift
@@ -0,0 +1,19 @@
+//
+//  Rating.swift
+//  hop&bop
+//
+//  Created by Amy Sywe on 8/7/19.
+//  Copyright © 2019 Haley Kim. All rights reserved.
+//
+
+import Foundation
+
+// TODO: Change this to a Realm model!
+
+class Rating {
+    var id: String?
+    var overall: Int?
+    var other: Int?
+    var review: String?
+    var created_at: Date?
+}
---
+++ hopbop/User.swift
@@ -0,0 +1,22 @@
+//
+//  User.swift
+//  hop&bop
+//
+//  Created by Amy Sywe on 8/7/19.
+//  Copyright © 2019 Haley Kim. All rights reserved.
+//
+
+import Foundation
+
+// TODO: Change this to a Realm model!
+
+class User {
+    var id: String?
+    var platform: String?
+    var full_name: String?
+    var email: String?
+    var phone: String?
+    var photo: String?
+    var last_rating: Date?
+    var created_at: Date?
+}
